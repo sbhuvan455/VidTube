d3baf488237ebca25a7292a5551afe3c37622c58
sbhuvan455 <sbhuvan455@gmail.com>
2023-11-26 23:13:58 +0530

    second commit

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_name } from "../constants.js";
+
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_name}`);
+        console.log(`Connected to Database: ${DB_name}`);
+    } catch (error) {
+        console.log("Error Connecting to Data Base", error);
+        process.exit(1);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './db/db.js';
 
 dotenv.config();
 
 const app = express();
+
+connectDB();
+
 const PORT = process.env.PORT||3000;
 
 app.listen(PORT, ()=>{
