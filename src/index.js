612cf4f8317c0eb110e6d4ec5a07561aa49431ba
sbhuvan455 <sbhuvan455@gmail.com>
2023-11-27 17:57:19 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './db/db.js';
+import { app } from './app.js';
 
 dotenv.config();
+const PORT = process.env.PORT || 8000;
 
-const app = express();
-
-connectDB();
-
-const PORT = process.env.PORT||3000;
-
-app.listen(PORT, ()=>{
-    console.log("listening to port 3000");
-});
+connectDB()
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Listening on port ${PORT}`);
+    })
+    app.on('error',(err)=>console.log(`Connected to Database, but server not listening ${err.message}`));
+})
+.catch((error)=>{
+    console.log("ERROR CONNECTING TO DATABASE: " + error.message);
+})
